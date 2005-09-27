fe4d00a6e5a96cef3962f8d123947cceb48f4bb6
Reggie Burnett <reggie.burnett@sun.com>
2005-09-27 14:19:42 +0000

    main.wxs - removed extra backslash from vs registry entries mysqlclient/AssemblyInfo.cs - updated version to 1.0.6 TestSuite/AssemblyInfo.cs - updated version to 1.0.6

--- mysqlclient/AssemblyInfo.cs
+++ mysqlclient/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.5.*")]
+[assembly: AssemblyVersion("1.0.6.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -79,6 +79,6 @@ using System.Runtime.CompilerServices;
 //
 [assembly: AssemblyDelaySign(false)]
 #if FINAL
-[assembly: AssemblyKeyFile(@"d:\work\connector-net\cnet.snk")]
+[assembly: AssemblyKeyFile(@"..\..\..\..\..\..\cnet.snk")]
 #endif
 [assembly: AssemblyKeyName("")]
