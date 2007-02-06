43c2d77f8f49cbf69f07cf48e1d093e7c41adba5
Reggie Burnett <reggie.burnett@sun.com>
2007-02-06 20:56:52 +0000

    fixed licensing banner on a couple of files.

--- Driver/Source/Connection.cs
+++ Driver/Source/Connection.cs
@@ -7,7 +7,7 @@
 // There are special exceptions to the terms and conditions of the GPL 
 // as it is applied to this software. View the full text of the 
 // exception in file EXCEPTIONS in the directory of this software 
-// distribution. 
+// distribution.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
