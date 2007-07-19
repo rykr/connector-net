7e6e554da7bc90a4c60c96a16726595804eb3de1
Reggie Burnett <reggie.burnett@sun.com>
2007-07-19 18:06:52 +0000

    bumped version #

--- Driver/Properties/AssemblyInfo.cs
+++ Driver/Properties/AssemblyInfo.cs
@@ -21,6 +21,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 //
 // General Information about an assembly is controlled through the following 
@@ -52,7 +53,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.0.7.1")]
+[assembly: AssemblyVersion("5.0.8")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- TestSuite/Properties/AssemblyInfo.cs
+++ TestSuite/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.0.7.0")]
+[assembly: AssemblyVersion("5.0.8")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
